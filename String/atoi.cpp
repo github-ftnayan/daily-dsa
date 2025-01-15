c7b007617bfdc80888b63162968a86ba46ded474
github-ftnayan <panchalnayann@gmail.com>
2025-01-15 23:42:33 +0530

    changed folder structure and added atoi.cpp

---
+++ String/atoi.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int myAtoi(char *s){
+    int sign = 1, res = 0, idx = 0;
+
+    while(s[idx] == ' ')
+        idx++;
+    
+    if(s[idx] == '-' or s[idx] == '+')
+        if(s[idx++] == '-')
+            sign = -1;
+    
+    while(s[idx] >= '0' and s[idx] <= '9'){
+        if(res > INT_MAX / 10 or (res == INT_MAX / 10 and s[idx] - '0' > 7))
+            return sign == 1 ? INT_MAX : INT_MIN;
+        
+        res = 10 * res + (s[idx++] - '0');
+    }
+    return res * sign;
+}
+
+int main(){
+    char s[] = "  -0012g4";
+    cout << myAtoi(s);
+    return 0;
+}
