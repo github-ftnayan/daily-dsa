be874841311989fc7c33bf6b5217db9416d2da76
github-ftnayan <panchalnayann@gmail.com>
2025-01-27 23:56:21 +0530

    added getLeaders.cpp

---
+++ Array/Basic/arrayLeaders.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<int> getLeaders(vector<int> &arr){
+    int size = arr.size();
+    vector<int> res;
+    int maxRight = arr[size - 1];
+    res.push_back(maxRight);
+
+    for(int i = size - 2; i >= size; i--){
+        if(arr[i] >= maxRight){
+            maxRight = arr[i];
+            res.push_back(arr[i]);
+        }
+    }
+    reverse(res.begin(), res.end());
+
+    return res;
+}
+
+int main(){
+    vector<int> arr = {16, 17, 4, 3, 5, 2};
+
+    vector<int> res = getLeaders(arr);
+
+    for(auto& r: res)
+        cout<< r << " ";
+
+    return 0;
+}
